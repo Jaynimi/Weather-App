cc9b81f29dc185c9e7203b89796aa0465ba5d949
Jaynimi <Eyeslikejays@yahoo.com>
2025-05-31 10:20:56 +0200

    move folders

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen flex items-center justify-center bg-gray-100`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
